30012ea84d12a09cac3a87a3c2a2cfaa26c0ecaf
bottles10 <henryaovare@gmail.com>
2023-09-07 02:06:47 -0400

    Refactored to three methods

--- caesar_cipher/caesar_cipher.rb
+++ caesar_cipher/caesar_cipher.rb
@@ -4,22 +4,25 @@ def caesar_cipher(string_arr, shift=1)
     unless shift != 0
         return 'Need shift factor to encode!(eg: 2)'
     else
-        string_arr_ord = string_ordinal(string_arr)
-        new_ord_with_shift = string_arr_ord.map do |char| 
-            if char.match?(/\d/) 
-               char.to_i.chr
-            elsif ('a'..'z').include?(char)
-                ( (char.ord - 'a'.ord + shift) % 26 + 'a'.ord ).chr
-            elsif ('A'..'Z').include?(char)
-                ( (char.ord - 'A'.ord + shift) % 26 + 'A'.ord).chr
-            else
-                char
-            end
-        end
-
-        encoded_string = new_ord_with_shift.join
-        encoded_string
+        encode_string(string_arr, shift)
     end
+end
+
+def encode_string(string_arr, shift)
+    string_arr_ord = string_ordinal(string_arr)
+    new_ord_with_shift = string_arr_ord.map do |char| 
+        if char.match?(/\d/) 
+            char.to_i.chr
+        elsif ('a'..'z').include?(char)
+            ( (char.ord - 'a'.ord + shift) % 26 + 'a'.ord ).chr
+        elsif ('A'..'Z').include?(char)
+            ( (char.ord - 'A'.ord + shift) % 26 + 'A'.ord).chr
+        else
+            char
+        end
+    end
+    encoded_string = new_ord_with_shift.join
+    encoded_string
 end
 
 def string_ordinal(string)
