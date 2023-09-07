f179d4051ff17ee6f9149aa16a353d5e08f0f1ad
bottles10 <henryaovare@gmail.com>
2023-09-07 02:04:54 -0400

    Implemented to wrap around alphabets only

--- caesar_cipher/caesar_cipher.rb
+++ caesar_cipher/caesar_cipher.rb
@@ -1,20 +1,32 @@
 # caesar cipher takes in a string and the shift factor and then outputs the ciphered string:
 
-def caesar_cipher(string_arr, shift) 
+def caesar_cipher(string_arr, shift=1) 
     unless shift != 0
         return 'Need shift factor to encode!(eg: 2)'
     else
-    string_arr_ord = string_ordinal(string_arr)
-    new_ord_with_shift = string_arr_ord.map { |ord| ord.to_s.match?(/\d/) ? ord + shift : ord }
-    encoded_string = new_ord_with_shift.map { |ord| ord.to_s.match?(/\d/) ? ord.chr : ord }
-    encoded_string.join
+        string_arr_ord = string_ordinal(string_arr)
+        new_ord_with_shift = string_arr_ord.map do |char| 
+            if char.match?(/\d/) 
+               char.to_i.chr
+            elsif ('a'..'z').include?(char)
+                ( (char.ord - 'a'.ord + shift) % 26 + 'a'.ord ).chr
+            elsif ('A'..'Z').include?(char)
+                ( (char.ord - 'A'.ord + shift) % 26 + 'A'.ord).chr
+            else
+                char
+            end
+        end
+
+        encoded_string = new_ord_with_shift.join
+        encoded_string
     end
 end
 
 def string_ordinal(string)
     string_arr = string.split('')
-    string_arr_ord = string_arr.map { |letter| letter.match?(/\w/) ? letter.ord : letter }    
+    string_arr.map { |letter| letter.match?(/\w/) ? letter : letter.ord.to_s }    
 end
 
+# p string_ordinal("what a string!")
 p caesar_cipher("What a string!", 5)
 #Output => "\\mfy f xywnsl!"
