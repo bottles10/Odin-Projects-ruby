6f8a10b9f671500ae5ad9f69cedb5ae93847befb
bottles10 <henryaovare@gmail.com>
2023-09-07 02:09:35 -0400

    Refactor if else to use case

--- caesar_cipher/caesar_cipher.rb
+++ caesar_cipher/caesar_cipher.rb
@@ -1,27 +1,31 @@
-# caesar cipher takes in a string and the shift factor and then outputs the ciphered string:
+# THIS PROGRAM USES THE CAESAR CIPHER TO ENCRYPT A MESSAGE.
 
-def caesar_cipher(string_arr, shift=1) 
+def caesar_cipher(message, shift=1) 
     unless shift != 0
         return 'Need shift factor to encode!(eg: 2)'
     else
-        encode_string(string_arr, shift)
+        encode_string(message, shift)
     end
 end
 
 def encode_string(string_arr, shift)
-    string_arr_ord = string_ordinal(string_arr)
-    new_ord_with_shift = string_arr_ord.map do |char| 
-        if char.match?(/\d/) 
+
+    string_arr_with_ord = string_ordinal(string_arr)
+    new_string_with_shift = string_arr_with_ord.map do |char| 
+
+        case char
+        when /\d/
             char.to_i.chr
-        elsif ('a'..'z').include?(char)
+        when /[a-z]/
             ( (char.ord - 'a'.ord + shift) % 26 + 'a'.ord ).chr
-        elsif ('A'..'Z').include?(char)
+        when /[A-Z]/
             ( (char.ord - 'A'.ord + shift) % 26 + 'A'.ord).chr
         else
             char
         end
     end
-    encoded_string = new_ord_with_shift.join
+
+    encoded_string = new_string_with_shift.join
     encoded_string
 end
 
@@ -30,6 +34,5 @@ def string_ordinal(string)
     string_arr.map { |letter| letter.match?(/\w/) ? letter : letter.ord.to_s }    
 end
 
-# p string_ordinal("what a string!")
-p caesar_cipher("What a string!", 5)
-#Output => "\\mfy f xywnsl!"
+print caesar_cipher("What a string!", 5)
+#Output => "Bmfy f xywnsl!"
